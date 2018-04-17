0969f571f103b26f46e7a106889cd9dc390b58bd
dani <danisako92@gmail.com>
2018-04-17 17:19:35 +0200

    primera version

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from cms import views
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'myproject.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+	url(r'^$','cms.views.index'),
+	url(r'^(.+)$','cms.views.muestra'),
     url(r'^admin/', include(admin.site.urls)),
 )
