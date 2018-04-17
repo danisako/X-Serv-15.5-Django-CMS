42faa2344fd92ef13241c609fe837baf9306d137
dani <danisako92@gmail.com>
2018-04-17 17:19:28 +0200

    primera version

--- cms/views.py
+++ cms/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Pages
 
-# Create your views here.
+
+
+
+def index(request):
+
+	respuesta = "<h2>Lista de páginas: </h2></br>"
+	paginas = Pages.objects.all()
+	
+	for p in paginas:
+		respuesta += "Nombre de pagina:" + p.name + "</br><li>contenido:" +str(p.page) + "    <li>ID:"+str(p.id)+"</br>"
+		respuesta += "<li><a href=" + str(p.id) + ">Enlace a la página de: " + p.name + "</a></li><br>"
+		
+	return HttpResponse(respuesta)
+
+
+def muestra(request,idpagina):
+	
+	try:
+		pagina = Pages.objects.get(id = idpagina)
+		respuesta = pagina.page
+	except Pages.DoesNotExist:
+		respuesta = "Error"
+
+	return HttpResponse(respuesta)
